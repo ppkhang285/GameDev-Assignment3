06888a7dabff8c11b5ddc1de6f2d6e013eed7510
Thái Ngọc Rạng <98004370+restom0@users.noreply.github.com>
2024-11-18 19:42:20 +0700

    add choose level

--- Assigment4/Assets/Scripts/TitleMenu/ScreenManager.cs
+++ Assigment4/Assets/Scripts/TitleMenu/ScreenManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ScreenManager : MonoBehaviour
 {
+    public GameObject level1Button;
+    public GameObject level2Button;
+    public GameObject level3Button;
+    public GameObject easyButton;
+    public GameObject mediumButton;
+    private int level { get; set; }
+    private string difficulty { get; set; }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,37 @@ public class ScreenManager : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnClickLevelButton(int level)
+    {
+        easyButton.SetActive(true);
+        mediumButton.SetActive(true);
+        this.level = level;
+    }
+    public void OnClickLevelOne()
+    {
+        OnClickLevelButton(1);
+    }
+    public void OnClickLevelTwo()
+    {
+        OnClickLevelButton(2);
+    }
+    public void OnClickLevelThree()
+    {
+        OnClickLevelButton(3);
+    }
+    void ChooseDifficulty(string difficulty)
+    {
+        this.difficulty = difficulty;
+        //change scene to DeckCollection
+        UnityEngine.SceneManagement.SceneManager.LoadScene("DeckCollection");
+    }
+    public void OnClickEasy()
+    {
+        ChooseDifficulty("Easy");
+    }
+    public void OnClickNormal()
+    {
+        ChooseDifficulty("Normal");
     }
 }
