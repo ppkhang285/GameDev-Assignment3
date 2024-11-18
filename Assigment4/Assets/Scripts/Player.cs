e8bcb3473be4c26fcb93e4ad01df4694999c3eb4
Phạm Phú Khang <ppkhang285@gmail.com>
2024-11-18 22:57:12 +0700

    Update lord sprites

--- Assigment4/Assets/Scripts/Player.cs
+++ Assigment4/Assets/Scripts/Player.cs
@@ -27,5 +27,10 @@ public class Player : MonoBehaviour
         Location = location;
 
         Data = new PlayerData(playerNo, type, characters.Select(p => p.GetComponent<Character>().Data).ToArray(), location);
+        
+        if (location.y != 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
     }
 }
